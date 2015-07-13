113cdc7c16fdd830a90c15442d027e0a6a6102ed
Rodrigo Ancavil <rancavil@gmail.com>
2015-07-12 23:39:42 -0400

    updated the version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except ImportError:
 
 distutils.core.setup(
 	name='pyconvert',
-	version = '0.5.0',
+	version = '0.5.1',
 	packages=['pyconvert','demos'],
 	author='Innovaser',
 	author_email='rancavil@innovaser.cl',
