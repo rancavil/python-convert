61e32c34f070fcd8d851849eee89f14ce7e7e6c5
Rodrigo Ancavil <rancavil@gmail.com>
2015-07-14 23:07:52 -0400

    new example json to python object

--- demos/json_2_obj_v3.py
+++ demos/json_2_obj_v3.py
@@ -19,32 +19,22 @@
 from pyconvert.pyconv import convertJSON2OBJ
 import json
 
-doc = {'userid':10010, 'username':'homerJ', 'projects' : [ {"seq" : 1, "name" : "API Develop"}, {"seq": 2, "name" : "Java Apps"}]}
-
-class Project(object):
-	seq = int
-	name = str
+doc = [{'username' : 'fred', 'age' : 20} , {'username' : 'barney', 'age' : 20}] 
 
 class User(object):
-	userid = int
 	username = str
-	projects = [Project]
+	age = int
 
-c = convertJSON2OBJ(User,doc)
+users = convertJSON2OBJ(User,doc)
 print "JSON original"
 print "*************"
 print json.dumps(doc, sort_keys=True, indent=2, separators=(',',': '))
 print
 print "Python obj     "
 print "***************"
-print "User data      "
+print "Users          "
 print "---------------"
-print "userid   : %d "%c.userid
-print "username : %s "%c.username
-print
-print "User's projects"
-print "---------------"
-for project in c.projects:
-	print "\tprojects.seq  : %d "%project.seq 
-	print "\tprojects.name : %s "%project.name
+for user in users:
+	print "\tuser.username : %s "%user.username
+	print "\tuser.age      : %s "%user.age
 	print
